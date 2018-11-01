05791199ca03b5707049662ce2c08ab92f3b9ae7
William Lu <rlu1@stuy.edu>
2018-11-01 17:02:19 -0400

    o

--- ls.c
+++ ls.c
@@ -5,8 +5,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-int main(){
-  DIR* my_dir = opendir(".");
+void print_dir(char* dir, int tab){
+  DIR* my_dir = opendir(dir);
 
   struct dirent* curr = readdir(my_dir);
   struct stat* file = malloc(sizeof(struct stat));
@@ -18,20 +18,33 @@ int main(){
     total_size += file->st_size;
     curr = readdir(my_dir);
   }
-  printf("Total Directory Size: %ld\n", total_size);
 
-  
+  char str[128] = "";
+  for (int i = 0; i < tab; i ++)
+    strcat(str, "\t");
+
+  printf("%sStatistics for directory: %s\n", str, dir);
+  printf("%sTotal Directory Size: %ld Bytes\n", str, total_size);
+
+  my_dir = opendir(dir);
   curr = readdir(my_dir);
+
   while (curr){
 
-    if (curr->d_type == DT_DIR)
-      printf("directory\t");
-    else
-      printf("regular file\t");
+    if (curr->d_type == DT_DIR){
+      printf("%sdirectory\t%s\n", str, curr->d_name);
+      if (strcmp(curr->d_name, ".") && strcmp(curr->d_name, ".."))
+        print_dir(curr->d_name, tab + 1);
+    } else
+      printf("%sregular file\t%s\n", str, curr->d_name);
 
-    printf("name: %s size: %ld\n", curr->d_name, file->st_size);
     curr = readdir(my_dir);
   }
 
   closedir(my_dir);
+}
+
+int main(){
+  print_dir(".", 0);
+  return 0;
 }
